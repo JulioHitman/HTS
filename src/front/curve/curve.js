0adc49af352082f28450d6d50b253bc711471eba
Julio <julio23927@gmail.com>
2021-08-08 21:38:41 -0300

    fixes

--- src/front/curve/curve.js
+++ src/front/curve/curve.js
@@ -16,14 +16,31 @@
 const superagent = require('superagent');
 
 //-----------CURV-----------
-const spotv = document.getElementById('spotv');
-const frp = document.getElementById('frp');
-const dolfutv = document.getElementById('dolfutv');
-const curv = document.getElementById('curv');
-const justv = document.getElementById('justv');
-const spreadv = document.getElementById('spreadv');
-const overv = document.getElementById('overv');
+const spotvUlt = document.getElementById('spotvUlt');
+const spotvMax = document.getElementById('spotvMax');
+const spotvMin = document.getElementById('spotvMin');
 
+const frpUlt = document.getElementById('frpUlt');
+const frpMAX = document.getElementById('frpMAX');
+const frpMin = document.getElementById('frpMin');
+
+const dolfutvUlt = document.getElementById('dolfutvUlt');
+const dolfutvMax = document.getElementById('dolfutvMax');
+const dolfutvMin = document.getElementById('dolfutvMin');
+
+const curvUlt = document.getElementById('curvUlt');
+const curvMax = document.getElementById('curvMax');
+const curvMin = document.getElementById('curvMin');
+
+const justvUlt = document.getElementById('justvUlt');
+const justvMax = document.getElementById('justvMax');
+const justvMin = document.getElementById('justvMin');
+
+const spreadv = document.getElementById('spreadv');
+
+const overvUlt = document.getElementById('overvUlt');
+const overvMax = document.getElementById('overvMax');
+const overvMin = document.getElementById('overvMin');
 
 const sixth = document.getElementById('sixth');
 const fifth = document.getElementById('fifth');
@@ -56,21 +73,40 @@ const negSixth = document.getElementById('negSixth');
       .get('https://fast-plains-96418.herokuapp.com/sendvalues')
       .end( (err, res) => {
         values = res.body.values;
-        console.log(values);
+        // console.log(values);
         // market_values = values["MARKET WATCH"];
         dolCurve = values["CURVA"];
         points = values["PONTOS"];
         refPoints = values["REF BLACK&SCHOLES"];
 
+        // console.log(dolCurve.CURVA[0]);
         
         //["CURVA"]
-        spotv.innerHTML = dolCurve.SPOT;
-        frp.innerHTML = dolCurve.FRP0;
-        dolfutv.innerHTML =  dolCurve.DOL1;
-        curv.innerHTML = dolCurve.CURVA;
-        justv.innerHTML = dolCurve.JUSTO;
+        spotvUlt.innerHTML = dolCurve.SPOT[0];
+        spotvMax.innerHTML = dolCurve.SPOT[1];
+        spotvMin.innerHTML = dolCurve.SPOT[2];
+
+        frpUlt.innerHTML = dolCurve.FRP0[0];
+        frpMAX.innerHTML = dolCurve.FRP0[1];
+        frpMin.innerHTML = dolCurve.FRP0[2];
+
+        dolfutvUlt.innerHTML =  dolCurve.DOL1[0];
+        dolfutvMax.innerHTML =  dolCurve.DOL1[1];
+        dolfutvMin.innerHTML =  dolCurve.DOL1[2];
+
+        curvUlt.innerHTML = dolCurve.CURVA[0];
+        curvMax.innerHTML = dolCurve.CURVA[1];
+        curvMin.innerHTML = dolCurve.CURVA[2];
+
+        justvUlt.innerHTML = dolCurve.JUSTO[0];
+        justvMax.innerHTML = dolCurve.JUSTO[1];
+        justvMin.innerHTML = dolCurve.JUSTO[2];
+
         spreadv.innerHTML = dolCurve["SPREAD CURVA/DOL"];
-        overv.innerHTML = dolCurve.OVER;
+
+        overvUlt.innerHTML = dolCurve.OVER[0];
+        overvMax.innerHTML = dolCurve.OVER[1];
+        overvMin.innerHTML = dolCurve.OVER[2];
 
 
         // CURVA.EUSCASADO
